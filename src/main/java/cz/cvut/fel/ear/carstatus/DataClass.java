a6991958007aa5e3799be66e958a4f28c134ec64
Vladyslav Babyč <babycvla@fel.cvut.cz>
2023-01-05 13:27:01 +0100

    update main-page

--- src/main/java/cz/cvut/fel/ear/carstatus/DataClass.java
+++ src/main/java/cz/cvut/fel/ear/carstatus/DataClass.java
@@ -1,18 +1,18 @@
 package cz.cvut.fel.ear.carstatus;
 
 public class DataClass {
-    private int numberOfMethodCalls;
+    private int numberOfLoadOfFileCalls;
     private int numberOfLoggerCalls;
     private int numberOfSimulationMethodCalls;
 
     public DataClass(int numberOfMethodCalls) {
-        this.numberOfMethodCalls = 0;
+        this.numberOfLoadOfFileCalls = 0;
         this.numberOfLoggerCalls = 0;
         this.numberOfSimulationMethodCalls = 0;
     }
 
     public int getNumberOfMethodCalls() {
-        return numberOfMethodCalls;
+        return numberOfLoadOfFileCalls;
     }
 
     public int getNumberOfLoggerCalls() {
