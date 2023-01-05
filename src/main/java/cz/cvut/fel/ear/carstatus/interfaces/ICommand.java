333076757a48ef04212fcf0ed2fda9223b1890c7
Klouček, Tomáš <kloucto2@fel.cvut.cz>
2023-01-05 12:46:25 +0100

    working command on rest api

--- src/main/java/cz/cvut/fel/ear/carstatus/interfaces/ICommand.java
+++ src/main/java/cz/cvut/fel/ear/carstatus/interfaces/ICommand.java
@@ -3,5 +3,5 @@ package cz.cvut.fel.ear.carstatus.interfaces;
 import java.util.ArrayList;
 
 public interface ICommand {
-    public void execute(ArrayList<String> parameters);
+    public void execute();
 }
