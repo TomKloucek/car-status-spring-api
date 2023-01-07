72ca648ce2ad9c242718dc2612a0578efe8e8f83
Klouček, Tomáš <kloucto2@fel.cvut.cz>
2023-01-07 19:31:05 +0100

    can specifically get roadtrip statistics

--- src/main/java/cz/cvut/fel/ear/carstatus/rest/StatisticsController.java
+++ src/main/java/cz/cvut/fel/ear/carstatus/rest/StatisticsController.java
@@ -1,26 +1,23 @@
 package cz.cvut.fel.ear.carstatus.rest;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import cz.cvut.fel.ear.carstatus.DataClass;
+import cz.cvut.fel.ear.carstatus.builders.StatisticsBuilder;
+import cz.cvut.fel.ear.carstatus.model.Roadtrip;
 import cz.cvut.fel.ear.carstatus.service.DriverService;
 import cz.cvut.fel.ear.carstatus.service.RoadTripService;
-import cz.cvut.fel.ear.carstatus.statistics.Statistics;
 import cz.cvut.fel.ear.carstatus.statistics.StatisticsFactory;
-import cz.cvut.fel.ear.carstatus.statistics.StatisticsFilter;
 import cz.cvut.fel.ear.carstatus.builders.StatisticsFilterBuilder;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.transaction.Transactional;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/rest/statistics")
@@ -28,12 +25,14 @@ public class StatisticsController {
 
     private final DriverService driverService;
     private final StatisticsFactory factory;
+    private final RoadTripService roadTripService;
 
 
     @Autowired
-    public StatisticsController(DriverService driverService, StatisticsFactory factory) {
+    public StatisticsController(DriverService driverService, StatisticsFactory factory, RoadTripService roadTripService) {
         this.driverService = driverService;
         this.factory = factory;
+        this.roadTripService = roadTripService;
     }
 
     @GetMapping(value = "/",produces = MediaType.APPLICATION_JSON_VALUE)
@@ -56,6 +55,20 @@ public class StatisticsController {
                 PrintWriter pw = new PrintWriter(sw);
                 ignored.printStackTrace(pw);
                 return sw.toString();
+        }
+    }
+
+    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public Object getStatisticsSpecific(@PathVariable Integer id) {
+        try {
+            List<Roadtrip> filtered = new ArrayList<>();
+            filtered.add(roadTripService.find(id));
+            return new StatisticsBuilder().numberOfKm(filtered).numberOfRoadtrips(filtered).averageSpeed(filtered).maxSpeed(filtered).build();
+        } catch (Exception ignored) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            ignored.printStackTrace(pw);
+            return sw.toString();
         }
     }
 }
