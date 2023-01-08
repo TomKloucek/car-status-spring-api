73a324510a6bb247f0432c3d1d94abb976c84c08
Vladyslav Babyč <babycvla@fel.cvut.cz>
2023-01-08 19:55:53 +0100

    add rest tests

---
+++ src/test/java/cz/cvut/fel/ear/carstatus/rest/CurrentBatteryGetTest.java
@@ -0,0 +1,29 @@
+package cz.cvut.fel.ear.carstatus.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CurrentBatteryGetTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void retrieveCurrentBattery() throws Exception {
+        this.mockMvc.perform(get("/rest/battery/").with(user("driver@fel.ear.cvut.cz").password("driver").roles("DRIVER")))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.inUsage").value(true));
+    }
+}
---
+++ src/test/java/cz/cvut/fel/ear/carstatus/rest/TyresGetTest.java
@@ -0,0 +1,33 @@
+package cz.cvut.fel.ear.carstatus.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TyresGetTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void retrieveAllTyres() throws Exception {
+        this.mockMvc.perform(get("/rest/tyre/").with(user("driver@fel.ear.cvut.cz").password("driver").roles("DRIVER")))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray());
+    }
+}
